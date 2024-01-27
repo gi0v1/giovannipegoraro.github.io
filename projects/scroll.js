ec75a0b82f97bcf0df45b9493af621b4851d7de4
gi0v1 <97177173+gi0v1@users.noreply.github.com>
2024-01-27 14:14:33 +0100

    Added arrow

---
+++ projects/scroll.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  var windowHeight = window.innerHeight;
+  var documentHeight = document.documentElement.scrollHeight;
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition + windowHeight < documentHeight) {
+    document.getElementById("arrow").style.display = "block";
+  } else {
+    document.getElementById("arrow").style.display = "none";
+  }
+}
