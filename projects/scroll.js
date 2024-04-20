64e56575b5dccf53bf82109e7a86604daf8ec520
gpego <97177173+gpego@users.noreply.github.com>
2024-04-20 17:31:28 +0200

    fine tuning

--- projects/scroll.js
+++ projects/scroll.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function() {
   var arrow = document.querySelector('#arrow');
   var scrollHeight = window.scrollY;
-  var scrollLimit = 500; // Change this value to the number of pixels you want
+  var scrollLimit = 10; // Change this value to the number of pixels you want
 
   if (scrollHeight > scrollLimit) {
       arrow.style.display = 'none';
